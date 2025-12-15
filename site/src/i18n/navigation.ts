fef965e7565a87b49a35ed22c660d50a4b8ec7be
Mathis TRUONG <mathis.truong@next-u.fr>
2025-12-16 00:40:58 +0100

    fix push

--- site/src/i18n/navigation.ts
+++
@@ -1,8 +0,0 @@
-// src/i18n/navigation.ts
-import {createSharedPathnamesNavigation} from "next-intl/navigation";
-
-export const locales = ["en", "fr"] as const;
-export type AppLocale = (typeof locales)[number];
-
-export const {Link, usePathname, useRouter} =
-  createSharedPathnamesNavigation({locales});
